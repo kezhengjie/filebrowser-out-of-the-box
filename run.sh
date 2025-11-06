dae74a4ddf3aa0a999fd38c9e37b1a8f0b85209b
kezhengjie <kezhengjie@minigame.vip>
2025-11-06 17:05:09 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 folder=~
 
 cd bin
-filebrowser config set --auth.method=noauth
-filebrowser.exe -a 0.0.0.0 -r $folder 
+chmod +x ./filebrowser
+./filebrowser config set --auth.method=noauth
+./filebrowser -a 0.0.0.0 -r $folder
